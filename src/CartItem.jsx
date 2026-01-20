9d2ea64ffedd1ce566f4ebfbc2509c0d44b74059
Jaxson <jaxson.h.richins.752@gmail.com>
2026-01-20 08:07:53 -0700

    Added checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -43,6 +43,10 @@ const CartItem = ({ onContinueShopping }) => {
     return totalCost;
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -68,7 +72,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
